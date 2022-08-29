a3709c729f772a44939fb0e94f31603c738f894f
tenDevs <clsuccess6@gmail.com>
2022-08-29 12:53:21 +0200

    added the spalch screen activity

--- app/src/main/java/com/takenotepro/takenotepro/activities/MainActivity.java
+++ app/src/main/java/com/takenotepro/takenotepro/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.takenotepro.takenotepro;
+package com.takenotepro.takenotepro.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.takenotepro.takenotepro.R;
 
 public class MainActivity extends AppCompatActivity {
 
