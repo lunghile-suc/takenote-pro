f6786760a7fff48238f82fa11368453fcc232255
tenDevs <clsuccess6@gmail.com>
2022-08-30 13:47:07 +0200

    Added functionality for the signUpActivity

--- app/src/main/java/com/takenotepro/takenotepro/activities/SignUpActivity.java
+++ app/src/main/java/com/takenotepro/takenotepro/activities/SignUpActivity.java
@@ -1,14 +1,24 @@
 package com.takenotepro.takenotepro.activities;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.takenotepro.takenotepro.R;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -37,5 +47,61 @@ public class SignUpActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                createAccount();
+
+            }
+        });
+    }
+
+    void createAccount(){
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String confirmPassword = confirmPassEditText.getText().toString();
+
+        boolean isValid = validate(email, password, confirmPassword);
+        if(!isValid){
+            return;
+        }
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUpActivity.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SignUpActivity.this, "Successfully created an account", Toast.LENGTH_SHORT).show();
+                            firebaseAuth.getCurrentUser().sendEmailVerification();
+                            firebaseAuth.signOut();
+                            finish();
+                        }else{
+                            Toast.makeText(SignUpActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    boolean validate(String email, String password, String confirmPassword){
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Invalid Email, Please enter valid email");
+            return false;
+        }
+
+        // validate the password
+        if(password.length() < 5){
+            passwordEditText.setError("Password must contain at least 5 characters");
+            return false;
+        }
+
+        //validate the confirm password
+        if(!password.equals(confirmPassword)){
+            confirmPassEditText.setError("Passwords don't match");
+            return false;
+        }
+        return true;
     }
 }
